0e98c9c15660ed0a7e2196b966819dbcc5b783f4
Edwin Ortega <edwin.ortega@gmail.com>
2026-01-16 14:26:03 -0800

    Debugger configuration

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "ab68fdbd-dce4-40ab-ad87-4082f3571f2a";
+private static final String ID = "ced92a68-0fe1-4d2d-893f-2cc59638b635";
 }
